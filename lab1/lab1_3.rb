a8952c40a785c93533dd787de33d3af46efe734e
anisimovalinaa <anisimova.linaa@yandex.ru>
2021-02-20 21:45:41 +0300

    Update lab1_3.rb
    Тернарный оператор

--- lab1/lab1_3.rb
+++ lab1/lab1_3.rb
@@ -36,15 +36,17 @@ l = gets.chomp.downcase
 # x = unless l != 'ruby' then puts 'Подлиза' 
 # 	else puts 'А скоро будет ruby' end
 
-case l
-when 'ruby'
-	puts 'Подлиза'
-when 'c#'
-	puts 'Не подлиза'
-when 'python'
-	puts 'Круто'
-when 'f#'
-	puts 'А скоро будет ruby'
-else
-	puts 'Я такого не знаю'
-end
+# case l
+# when 'ruby'
+# 	puts 'Подлиза'
+# when 'c#'
+# 	puts 'Не подлиза'
+# when 'python'
+# 	puts 'Круто'
+# when 'f#'
+# 	puts 'А скоро будет ruby'
+# else
+# 	puts 'Я такого не знаю'
+# end
+
+l == 'ruby' ? puts('Подлиза') : puts('А я такого не знаю')
