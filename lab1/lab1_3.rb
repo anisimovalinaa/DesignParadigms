20ddcfbf94ab733b391f5de4759b47fb5492a8d4
anisimovalinaa <anisimova.linaa@yandex.ru>
2021-02-20 21:40:22 +0300

    Update lab1_3.rb
    case

--- lab1/lab1_3.rb
+++ lab1/lab1_3.rb
@@ -33,6 +33,18 @@ l = gets.chomp.downcase
 
 # puts 'Подлиза' unless l != 'ruby'
 
-x = unless l != 'ruby' then puts 'Подлиза' 
-	else puts 'А скоро будет ruby' end
+# x = unless l != 'ruby' then puts 'Подлиза' 
+# 	else puts 'А скоро будет ruby' end
 
+case l
+when 'ruby'
+	puts 'Подлиза'
+when 'c#'
+	puts 'Не подлиза'
+when 'python'
+	puts 'Круто'
+when 'f#'
+	puts 'А скоро будет ruby'
+else
+	puts 'Я такого не знаю'
+end
