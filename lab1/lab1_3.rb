9073e7958e27a062df02e28bd251790f30026206
anisimovalinaa <anisimova.linaa@yandex.ru>
2021-02-20 21:02:53 +0300

    Update lab1_3.rb
    if elsif else

--- lab1/lab1_3.rb
+++ lab1/lab1_3.rb
@@ -1,9 +1,19 @@
 puts 'Какой Ваш любимый язык программирования?'
 l = gets.chomp.downcase
 
-if l == 'ruby' 
+# if l == 'ruby' 
+# 	puts 'Подлиза'
+# else 
+# 	puts 'Я такого не знаю'
+# end
+		
+if l == 'ruby'
 	puts 'Подлиза'
+elsif l == 'c#'
+	puts 'Не подлиза'
+elsif l == 'python'
+	puts 'Круто'
 else 
 	puts 'Я такого не знаю'
 end
-		
+	
