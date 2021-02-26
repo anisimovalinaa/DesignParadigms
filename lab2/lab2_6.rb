9facf61608071f3b68374ab995f599916cc0a27b
anisimovalinaa <anisimova.linaa@yandex.ru>
2021-02-26 20:42:02 +0300

    Update lab2_6.rb

--- lab2/lab2_6.rb
+++ lab2/lab2_6.rb
@@ -2,6 +2,13 @@ def task1 l
 	l[l.index(l.max)+1..]
 end
 
+def task13 l
+	ind = l.index(l.min)
+	# l + l[..ind-1]
+	l - l[..ind-1] + l[..ind-1]
+end
+
 l = gets.split().map { |e| e.to_i }
 
-puts "#{task1 l}"
+# puts "#{task1 l}"
+puts "#{task13 l}"
