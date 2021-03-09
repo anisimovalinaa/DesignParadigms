a128ec5045a32f869987fb8e745fc44bb7052111
anisimovalinaa <anisimova.linaa@yandex.ru>
2021-03-09 21:43:00 +0300

    Update lab2_12.rb
    task13

--- lab2/lab2_12.rb
+++ lab2/lab2_12.rb
@@ -4,10 +4,14 @@ def read_str
 	list_str
 end
 
-def task list_str
+def task12 list_str
 	list_str.sort { |a, b| a.size <=> b.size }.reverse
+end
+
+def task13 list_str
+	list_str.sort { |a, b| a.split.size <=> b.split.size }
 end
 
 list_str = read_str
 
-puts "#{task list_str}"
+puts "#{task13 list_str}"
