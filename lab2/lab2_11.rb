b90507175ff5f85070ae087b5056ca4f7089d71a
anisimovalinaa <anisimova.linaa@yandex.ru>
2021-03-09 20:59:52 +0300

    Update lab2_11.rb
    task9

--- lab2/lab2_11.rb
+++ lab2/lab2_11.rb
@@ -10,6 +10,12 @@ def task1 str
 	real_digits.max
 end
 
+def task9 str
+	real_digits = str.scan(/-?[\d]+\.[\d]+/)
+	real_digits = real_digits.map { |el| el.to_f }
+	real_digits.min
+end
+
 str = read_str
 
-puts "#{task1 str}"
+puts "#{task9 str}"
