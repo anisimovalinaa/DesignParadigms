e03512dc1a66c15fa5de8fc6cc07210099d523f0
anisimovalinaa <anisimova.linaa@yandex.ru>
2021-03-09 19:03:27 +0300

    Update lab2_4.rb
    Задание 5

--- lab2/lab2_4.rb
+++ lab2/lab2_4.rb
@@ -1,9 +1,69 @@
-def read_list_from_file
+def method1 n
 	l = []
-	File.open('file.txt') { |file| l = file.read()}
+	for i in 0..n-1
+		el = $stdin.gets.to_i
+		l += [el]
+	end
+	l
+end
+
+def method2 n
+	l = []
+	for i in 0..n-1
+		el = $stdin.gets.to_i
+		l << el
+	end
+	l
+end
+
+def method3 n
+	l = []
+	for i in 0..n-1
+		el = $stdin.gets.to_i
+		l.concat([el])
+	end
+	l
+end
+
+def method4 n
+	l = []
+	for i in 0..n-1
+		el = $stdin.gets.to_i
+		l.insert(l.size, el)
+	end
+	l
+end
+
+def method5 n
+	l = []
+	for i in 0..n-1
+		el = $stdin.gets.to_i
+		l[l.size] = el
+	end
+	l
+end
+
+def read_list_from_file path
+	l = []
+	File.open(path) { |file| l = file.read()}
 	l = l.split().map { |e| e.to_i }
 	l
 end 
 
-l = read_list_from_file
-puts l
+# l = read_list_from_file '/home/alina/Документы/file.txt'
+
+case ARGV[1].to_i
+when 1
+	l = method1 ARGV[0].to_i
+when 2
+	l = method2 ARGV[0].to_i
+when 3
+	l = method3 ARGV[0].to_i
+when 4
+	l = method4 ARGV[0].to_i
+when 5
+	l = method5 ARGV[0].to_i
+when 6
+	l = read_list_from_file ARGV[2]
+end
+puts "#{l}"
