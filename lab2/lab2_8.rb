14d80d9031fdb08251af514d42fa10daf661031e
anisimovalinaa <anisimova.linaa@yandex.ru>
2021-03-09 19:14:20 +0300

    Update lab2_8.rb

--- lab2/lab2_8.rb
+++ lab2/lab2_8.rb
@@ -1,9 +1,8 @@
 def read_list_from_file
-	f = File.open('file.txt')
-
-	l = f.read()
+	l = []
+	File.open('file.txt') { |file| l = file.read()}
 	l.split().map { |e| e.to_i }
 end 
 
 l = read_list_from_file
-puts "#Максимум = {l.max}"
+puts "Максимум = #{l.max}"
