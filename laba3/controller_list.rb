7c53065996464ba5bb5a18852e9cc3c7cb79499c
anisimovalinaa <62428300+anisimovalinaa@users.noreply.github.com>
2021-05-06 23:07:40 +0300

    controller_list

---
+++ laba3/controller_list.rb
@@ -0,0 +1,33 @@
+require_relative 'controller'
+
+class Controller_list
+  include Controller
+  def show_view
+
+  end
+
+  def show_list
+
+  end
+
+  def choose_instance(number)
+
+  end
+
+  def add_instance(*args)
+
+  end
+
+  def show_instance
+
+  end
+
+  def delete_instance
+
+  end
+
+  def close_view
+
+  end
+
+end
